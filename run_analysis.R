f64b2b67b57bb83e0876216cb5cb4b60476a6912
Scott Cochran <scott.cochran@verizon.net>
2014-09-18 23:55:24 -0400

    baby steps...

--- run_analysis.R
+++ run_analysis.R
@@ -12,7 +12,7 @@ library(data.table)
 
 ## Load Data Sets
 # Load Activity Labels (not yet as factors)
-dt_activities <- as.data.table( read.table(file = "UCI HAR Dataset//activity_labels.txt", stringsAsFactors = F) )
+dt_activities <- as.data.table( read.table(file = "UCI HAR Dataset//activity_labels.txt", stringsAsFactors = F, col.names = c("Level", "Activity")) )
 
 # Load Training Data
 dt_subject_train <- as.data.table( read.table(file = "UCI HAR Dataset//train/subject_train.txt") )
@@ -49,65 +49,15 @@ tail(dt_tidy[,list(V1,V2,V3,V4)])
 # TODO: Prune down the variables
 
 # 3. Descriptively name the activities in the data set
-dt_tidy[[2]] <- factor(dt_tidy[[2]], levels = dt_activities$V1, labels = dt_activities$V2)
+dt_tidy[[2]] <- factor(dt_tidy[[2]], levels = dt_activities$Level, labels = dt_activities$Activity)
 
 
 # 4. Descriptively label the variables in the data set
 # TODO: Get these using read.table, filter for mean, strings.as.factor
-variable_labels <- c("tBodyAcc-mean()-X",
-                     "tBodyAcc-mean()-Y",
-                     "tBodyAcc-mean()-Z",
-                     "tGravityAcc-mean()-X",
-                     "tGravityAcc-mean()-Y",
-                     "tGravityAcc-mean()-Z",
-                     "tBodyAccJerk-mean()-X",
-                     "tBodyAccJerk-mean()-Y",
-                     "tBodyAccJerk-mean()-Z",
-                     "tBodyGyro-mean()-X",
-                     "tBodyGyro-mean()-Y",
-                     "tBodyGyro-mean()-Z",
-                     "tBodyGyroJerk-mean()-X",
-                     "tBodyGyroJerk-mean()-Y",
-                     "tBodyGyroJerk-mean()-Z",
-                     "tBodyAccMag-mean()",
-                     "tGravityAccMag-mean()",
-                     "tBodyAccJerkMag-mean()",
-                     "tBodyGyroMag-mean()",
-                     "tBodyGyroJerkMag-mean()",
-                     "fBodyAcc-mean()-X",
-                     "fBodyAcc-mean()-Y",
-                     "fBodyAcc-mean()-Z",
-                     "fBodyAcc-meanFreq()-X",
-                     "fBodyAcc-meanFreq()-Y",
-                     "fBodyAcc-meanFreq()-Z",
-                     "fBodyAccJerk-mean()-X",
-                     "fBodyAccJerk-mean()-Y",
-                     "fBodyAccJerk-mean()-Z",
-                     "fBodyAccJerk-meanFreq()-X",
-                     "fBodyAccJerk-meanFreq()-Y",
-                     "fBodyAccJerk-meanFreq()-Z",
-                     "fBodyGyro-mean()-X",
-                     "fBodyGyro-mean()-Y",
-                     "fBodyGyro-mean()-Z",
-                     "fBodyGyro-meanFreq()-X",
-                     "fBodyGyro-meanFreq()-Y",
-                     "fBodyGyro-meanFreq()-Z",
-                     "fBodyAccMag-mean()",
-                     "fBodyAccMag-meanFreq()",
-                     "fBodyBodyAccJerkMag-mean()",
-                     "fBodyBodyAccJerkMag-meanFreq()",
-                     "fBodyBodyGyroMag-mean()",
-                     "fBodyBodyGyroMag-meanFreq()",
-                     "fBodyBodyGyroJerkMag-mean()",
-                     "fBodyBodyGyroJerkMag-meanFreq()",
-                     "angle(tBodyAccMean,gravity)",
-                     "angle(tBodyAccJerkMean),gravityMean)",
-                     "angle(tBodyGyroMean,gravityMean)",
-                     "angle(tBodyGyroJerkMean,gravityMean)",
-                     "angle(X,gravityMean)",
-                     "angle(Y,gravityMean)",
-                     "angle(Z,gravityMean)"
-                     )
+dt_features <- as.data.table( read.table(file = "UCI HAR Dataset/features.txt", stringsAsFactors = F) )
+library(sqldf)
+dt_feature_means <- sqldf("select * from dt_features where V2 like '%mean%' or V2 like '%std%'")
+
 
 # 5. Create a second tidy data set with the average of each
 #    variable for each activity and each subject
